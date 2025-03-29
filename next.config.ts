2012c390ae7803b518afb165966f8a041d7b9956
Durvesh kumar <durvesh88999@gmail.com>
2025-03-29 21:28:31 +0530

    Added code

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   distDir: '.next', // Custom output directory
-  reactStrictMode: true,
-  swcMinify: true,
+  // reactStrictMode: true,
+  // swcMinify: true,
 };
 
 export default nextConfig;
